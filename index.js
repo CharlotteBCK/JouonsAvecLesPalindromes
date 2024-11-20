4f35b3741fbe666eedc38495c997de49c2515904
CharlotteBCK <charlotte44300@gmail.com>
2024-11-20 11:29:37 +0100

    Ajout fonction isValideDate qui permet de verifier  les mois/années

--- index.js
+++ index.js
@@ -1,37 +1,59 @@
-function separateDate (date){
+function separateDate(date) {
 
-    let dayNumber =""
+    let dayNumber = ""
     let monthNumber = ""
     let yearNumber = ""
     let datePart = ""
 
-    for(let index = 0; index < date.length; index++){
-        if (date[index]=== "/" && index===2){
+    for (let index = 0; index < date.length; index++) {
+        if (date[index] === "/" && index === 2) {
             dayNumber = datePart
-            datePart =""
+            datePart = ""
         }
-        if (date[index]=== "/" && index===5){
+        if (date[index] === "/" && index === 5) {
             monthNumber = datePart
             datePart = ""
 
         }
-        if (date[index]=== "/"){
+        if (date[index] === "/") {
             continue
 
         }
         datePart += date[index]
 
-        if(index === date.length -1 ){
+        if (index === date.length - 1) {
             yearNumber = datePart
             datePart = ""
 
         }
     }
 
-    console.log(dayNumber)
-    console.log(monthNumber)
-    console.log(yearNumber)
+    return {
+        dayNumber: dayNumber,
+        monthNumber: monthNumber,
+        yearNumber: yearNumber
 
+    }
 }
 
 separateDate("03/04/2001")
+
+
+function isValideDate(date) {
+
+    const dateSeparated = separateDate(date)
+
+    if(dateSeparated.monthNumber < 1 || 12 < dateSeparated.monthNumber){
+        return false
+    } 
+    if (dateSeparated.yearNumber < 999 || 9999 <dateSeparated.yearNumber){
+        return false
+
+    }
+
+    return true
+
+}
+
+
+console.log(isValideDate("03/14/2001"))
